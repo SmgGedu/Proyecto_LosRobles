936a90808bb5629db5e7c0041cb8921aaa6468c7
SmgGedu <chacaltana650@gmail.com>
2026-05-13 16:39:13 -0500

    front

--- app-frontend/src/api/axiosConfig.js
+++ app-frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    baseURL: 'api-losrobles-backend-bheecjfpg3axbnem.brazilsouth-01.azurewebsites.net'
 });
 
 // Este interceptor pegará el Token JWT automáticamente en cada petición que hagamos
